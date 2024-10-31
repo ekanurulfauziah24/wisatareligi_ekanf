865ef2ee073837a6ff41616544b644b65560b40e
ekanurulfauziah24 <ekanf242@gmail.com>
2024-10-31 18:39:15 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,9 +7,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $pesan = $_POST['pesan'];
     $foto = $_FILES['foto']['name'];
+
     
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($foto);
+
+    
+    
        
         $query = "INSERT INTO wisatareligi (nama, email, pesan, foto) VALUES ('$nama', '$email', '$pesan', '$foto')";
 
